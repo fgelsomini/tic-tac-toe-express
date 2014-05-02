3ebae7f645714fbcec8529a9c8bf5c01bae1dccc
= <=>
2014-05-01 21:22:03 -0400

    Add port switch for production

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.get('/', function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
