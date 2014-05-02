84b8a00c97b6b38c7b5aeaa83b0d316ee50f198c
= <=>
2014-05-01 21:12:17 -0400

    Possible fix for Heroku deploy

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+  res.sendfile('public/index.html');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function() {
